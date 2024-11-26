68b4fa117bebaf4eb4895abed881ccacac436b55
Rafael Krüger Ferreira <86074494+Raafaeel@users.noreply.github.com>
2024-11-25 22:25:03 -0300

    Teste

--- Unity-master/Assets/Scripts/Core/UI/MainMenu/DifficultySelector.cs
+++ Unity-master/Assets/Scripts/Core/UI/MainMenu/DifficultySelector.cs
@@ -1,22 +1,24 @@
-using UnityEngine;
-using UnityEngine;
-using UnityEngine.UI;
+// using UnityEngine;
+// using UnityEngine;
+// using UnityEngine.UI;
+// namespace Core{
 
-public class DifficultySelector : MonoBehaviour
-{
-    public void SetEasy()
-    {
-        DifficultyManager.Instance.SetDifficulty(Difficulty.Easy);
-    }
+// public class DifficultySelector : MonoBehaviour
+// {
+//     public void SetEasy()
+//     {
+//         DifficultyManager.Instance.SetDifficulty(Difficulty.Easy);
+//     }
 
-    public void SetMedium()
-    {
-        DifficultyManager.Instance.SetDifficulty(Difficulty.Medium);
-    }
+//     public void SetMedium()
+//     {
+//         DifficultyManager.Instance.SetDifficulty(Difficulty.Medium);
+//     }
 
-    public void SetHard()
-    {
-        DifficultyManager.Instance.SetDifficulty(Difficulty.Hard);
-    }
-}
+//     public void SetHard()
+//     {
+//         DifficultyManager.Instance.SetDifficulty(Difficulty.Hard);
+//     }
+// }
 
+// }
