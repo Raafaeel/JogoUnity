fbde5de9448defa8736e0a9eba86848551093ee2
Rafael Krüger Ferreira <86074494+Raafaeel@users.noreply.github.com>
2025-05-30 21:28:32 -0300

    Difficulty Manager Creation
    Making the initial additions of a Difficulty Manager. The code isnt finished yet

--- Unity-master/Assets/Scripts/Core/Managers/Game.cs
+++ Unity-master/Assets/Scripts/Core/Managers/Game.cs
@@ -15,6 +15,7 @@ namespace Core
         public static MenuManager Menu { get; private set; }
         public static BattleManager Battle { get; private set; }
         public static CutsceneManager Cutscenes { get; private set; }
+        public static DifficultyManager Difficulty { get; private set; }
         public static MapManager World { get; private set; }
         public static Player Player { get; private set; }
         public static GameState State => stateManager.State;
@@ -22,6 +23,7 @@ namespace Core
         [SerializeField] private Map startingMap;
         [SerializeField] private GameObject playerPrefab;
         [SerializeField] private Vector2Int startingCell;
+        [SerializeField] private Vector2Int currenteDifficulty;
 
         private void Awake()
         {
@@ -40,34 +42,13 @@ namespace Core
             Battle = new BattleManager(stateManager);
             Cutscenes = new CutsceneManager(stateManager);
             World = new MapManager(stateManager, startingMap);
+            Difficulty = new DifficultyManager(stateManager);
         }
 
         private void SpawnPlayer()
         {
             Player = Instantiate(playerPrefab).GetComponent<Player>();
             Player.transform.position = startingCell.Center2D();
-        }
-
-        private void StartDifficulty()
-        {
-            // Acesso ao enum Difficulty através do namespace Battle
-            Difficulty difficulty = DifficultyManager.Instance.currentDifficulty;
-
-            switch (difficulty)
-            {
-                case Difficulty.Easy:
-                    Debug.Log("Dificuldade: Fácil");
-                    // Configurações para fácil
-                    break;
-                case Difficulty.Medium:
-                    Debug.Log("Dificuldade: Médio");
-                    // Configurações para médio
-                    break;
-                case Difficulty.Hard:
-                    Debug.Log("Dificuldade: Difícil");
-                    // Configurações para difícil
-                    break;
-            }
         }
     }
 }
