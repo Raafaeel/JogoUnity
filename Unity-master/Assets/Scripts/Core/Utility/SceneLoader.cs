efd1e303d1ddd84959bf74f2ff89187d78f0082e
Rafael Krüger Ferreira <86074494+Raafaeel@users.noreply.github.com>
2025-06-13 23:16:41 -0300

    Battle Transitions Based on Stage

--- Unity-master/Assets/Scripts/Core/Utility/SceneLoader.cs
+++ Unity-master/Assets/Scripts/Core/Utility/SceneLoader.cs
@@ -7,35 +7,32 @@ namespace Core
 {
     public static class SceneLoader
     {
-        private static int battleSceneBuildIndex = 2; // Índice da cena de batalha
+        private static int battleSceneBuildIndex = 4; // Índice da cena de batalha
         private static Vector2 savedPlayerLocation;
+        private static int savedPreviousSceneIndex; // Nova variável para armazenar a cena anterior
 
-        public static void LoadBattleScene()
+    public static void LoadBattleScene()
         {
-            // Salva o estado atual do jogo antes de carregar a cena de batalha
-            GameObject.DontDestroyOnLoad(Game.World.Map);
-            Game.World.Map.gameObject.SetActive(false);
-            savedPlayerLocation = Game.Player.CurrentCell.Center2D();
-            SceneManager.LoadScene(battleSceneBuildIndex);
-            SceneManager.sceneLoaded += DisablePlayerObject;
+        // Salva a cena atual como anterior
+        savedPreviousSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        
+        // Obtém a cena de combate do mapa atual
+        int battleSceneIndex = Game.World.Map.BattleSceneIndex;
+        
+        // Resto do código permanece igual...
+        GameObject.DontDestroyOnLoad(Game.World.Map);
+        Game.World.Map.gameObject.SetActive(false);
+        savedPlayerLocation = Game.Player.CurrentCell.Center2D();
+        
+        SceneManager.LoadScene(battleSceneIndex); // Usa o índice do mapa
+        SceneManager.sceneLoaded += DisablePlayerObject;
         }
 
-        public static void LoadNextSceneAfterBattle()
+        // Novo método para retornar à cena anterior
+        public static void ReturnToPreviousScene()
         {
-            // Calcula o próximo índice da cena
-            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-            int nextSceneIndex = currentSceneIndex + 1;
-
-            // Verifica se o próximo índice está dentro da lista de cenas no Build Settings
-            if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings)
-            {
-                Debug.LogWarning("Nenhuma próxima cena disponível no Build Settings!");
-                return; // Não faz nada se não houver próxima cena
-            }
-
-            // Carrega a próxima cena
             SceneManager.sceneLoaded += RestorePlayerPosition;
-            SceneManager.LoadScene(nextSceneIndex);
+            SceneManager.LoadScene(savedPreviousSceneIndex);
         }
 
         private static void RestorePlayerPosition(Scene scene, LoadSceneMode mode)
